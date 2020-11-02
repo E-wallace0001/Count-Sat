1b258748571e641bfb0bb6c2aebe0301d9e26461
E-wallace0001 <69038841+E-wallace0001@users.noreply.github.com>
2020-11-02 14:46:56 +0000

    Delete run.sh

--- run.sh
+++
@@ -1,1 +0,0 @@
-gcc   -g -pg map.c stest.c  h_table.c cpu_pool.c bfs_test.c index.c llist.c var_pos.c clause_func.c cnf_read.c infini_tree.c m_map.c -o stest -lm -lgmp -lpthread -pthread -D_REENTRANT
